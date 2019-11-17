f44be11b3291d35c69d87425c91a8ec7a1e22241
Linchen Chen <lin.chen@student.unsw.edu.au>
2019-11-17 13:37:11 +1100

    19t2 not work

--- ass/ass3/ass3.sql
+++ ass/ass3/ass3.sql
@@ -24,40 +24,40 @@ group by class_id;
 
 --q7 sections
 -- binary string OR funciton
--- create or replace function BinaryOR(left_ text, right_ text) returns text
--- as
--- $$
--- declare
---   result text = '';
---   r_arr text[];
---   l_arr text[];
---   i int;
--- begin
---   if left_ = '' then
---     return right_;
---   end if;
+create or replace function BinaryOR(left_ text, right_ text) returns text
+as
+$$
+declare
+  result text = '';
+  r_arr text[];
+  l_arr text[];
+  i int;
+begin
+  if left_ = '' then
+    return right_;
+  end if;
 
---   select regexp_split_to_array(left_,'') into l_arr;
---   select regexp_split_to_array(right_,'') into r_arr;
---   for i in 1..11
---   loop
---     if l_arr[i] = '1' or r_arr[i] = '1' then
---       result = result || '1';
---     else
---       result = result || '0';
---     end if;
---   end loop;
---   return result;
--- end;
--- $$ language plpgsql;
+  select regexp_split_to_array(left_,'') into l_arr;
+  select regexp_split_to_array(right_,'') into r_arr;
+  for i in 1..11
+  loop
+    if l_arr[i] = '1' or r_arr[i] = '1' then
+      result = result || '1';
+    else
+      result = result || '0';
+    end if;
+  end loop;
+  return result;
+end;
+$$ language plpgsql;
 
--- drop aggregate if exists BinaryORRows(text);
+drop aggregate if exists BinaryORRows(text);
 
--- create aggregate BinaryORRows(text)(
---   stype = text,
---   initcond = '',
---   sfunc = BinaryOR
--- );
+create aggregate BinaryORRows(text)(
+  stype = text,
+  initcond = '',
+  sfunc = BinaryOR
+);
 
 --view for room used in 19T3
 create or replace view T3Rooms
@@ -69,7 +69,7 @@ select
   m.start_time as starting,
   m.end_time as ending,
   m.weeks_binary as weeks
-from meetings m 
+from meetings m  
 join
   (
     select c.id as class_id, T3C.name as term from classes c 
@@ -138,14 +138,22 @@ r.code, m.day, m.start_time, m.end_time;
 
 -- better view for room used in 19T3
 -- no double booking, no overlap class which has the same start_time and end_time
--- create or replace view T3Rooms2
--- as
--- select room,day,starting,ending, BinaryORrows(weeks) from T3rooms 
--- group by room, day, starting, ending;
+create or replace view T3Rooms2
+as
+select room,day,starting,ending, BinaryORrows(weeks) as weeks from T3rooms 
+group by room, day, starting, ending;
+
+create or replace view T2Rooms2
+as
+select room,day,starting,ending, BinaryORrows(weeks) as weeks from T2rooms 
+group by room, day, starting, ending;
+
+create or replace view T1Rooms2
+as
+select room,day,starting,ending, BinaryORrows(weeks) as weeks from T1rooms 
+group by room, day, starting, ending;
 
 
 -- Count total Room- 508
-select sum(count) from (
-(select count(*) from rooms where code ~* '^K-.*' group by id)) as Foo;
 
 
--- ass/ass3/q7.py
+++ ass/ass3/q7.py
@@ -1,6 +1,124 @@
 import psycopg2
 import sys
 
+
+DAYMAP = {
+  'Mon':1,
+  'Tue':2,
+  'Wed':3,
+  'Thu':4,
+  'Fri':5,
+  'Sat':6,
+  'Sun':7
+}
+
+DAYMAP2 = {
+  1:'Mon',
+  2:'Tue',
+  3:'Wed',
+  4:'Thu',
+  5:'Fri',
+  6:'Sat',
+  7:'Sun'
+}
+
+class Room:
+  def __init__(self, room, day, starting, ending, wk):
+    self.room = room
+    self.tt = {}
+    day = DAYMAP[day]
+    #tt is timetable -> list of (day, start, end, weeks binary)
+    self.tt[day] = [(day, starting, ending, wk)]
+  
+  def __str__(self):
+    string = "{}\n".format(self.room)
+    for num_day, classes in self.tt.items():
+      string += "  {}\n".format(DAYMAP2[num_day])
+      for day, start, end, wk in classes:
+        string += "    {}-{}: {}\n".format(start, end, wk)
+    
+    return string
+  
+  def get_avg_hour(self):
+    t_hour = 0
+    for day_cls in self.tt.values():
+      for day, starting, ending, wk in day_cls:
+        t_hour += count_time(ending - starting, wk)
+  
+    return t_hour/10
+    
+  def add(self, day, starting ,ending ,wk):
+    day = DAYMAP[day]
+    if day not in self.tt.keys():
+      self.tt[day] = [(day, starting, ending, wk)]
+    else:
+      #remove overlap class
+      temp_cls = [(day, starting, ending, wk)]
+      for cl in self.tt[day]:
+        temp_cls = self.solve(temp_cls, cl)
+
+      #update timetable
+      for cls in temp_cls:
+        self.tt[day].append(cls)
+  
+  #only called from add -> day is formatted from DAYMAP already
+  def solve(self,temp_cls, cl):
+    new_cls = []
+    old_day, old_starting, old_ending, old_wk = cl
+    
+    for day, starting, ending, wk in temp_cls:
+      #if overlap
+      if old_starting <= starting and starting < old_ending:
+        #diff wk
+        count, diff_cls = self.get_exceed_with_diff_wks(day, starting, ending, wk, cl)
+        if count > 0:
+          new_cls.append(diff_cls)
+        
+        #exceed same week
+        count, same_cls = self.get_exceed_with_same_wks(day, starting, ending, wk, cl)
+        if count > 0:
+#           print(self.room, cl, (day, starting, ending, wk))
+          new_cls.append(same_cls)
+    
+    if not new_cls:
+      return temp_cls
+    else:
+      return new_cls
+  
+  def get_exceed_with_same_wks(self, day, starting, ending, wk, cl):
+    old_day, old_starting, old_ending, old_wk = cl
+    #check exceed time
+    exceed_time = ending - old_ending
+    if exceed_time <= 0:
+      return -1, (day,0,0,"00000000000")
+    
+    #exceed_time > 0 
+    new_starting = old_ending
+    
+    result = ""
+    count = 0
+    for i in range(11):
+      if wk[i] == "1" and old_wk[i] == "1":
+        result += '1'
+        count += 1
+      else:
+        result += '0'
+
+    return count, (day, new_starting, ending, result)
+    
+  def get_exceed_with_diff_wks(self, day,starting, ending, wk, cl):
+    old_day, old_starting, old_ending, old_wk = cl
+    result = ""
+    count = 0
+    for i in range(11):
+      if wk[i] == "1" and old_wk[i] == "0":
+        result += '1'
+        count += 1
+      else:
+        result += '0'
+    
+    return count, (day, starting, ending, result)
+
 def q7(conn, key):
   # select term
   if key == '19T1':
@@ -14,27 +132,20 @@ def q7(conn, key):
 
   cur = conn.cursor()
   query = '''
-  select room, day, starting, ending, weeks from T{}rooms;
+  select room, day, starting, ending, weeks from T{}rooms2;
   '''.format(key)
 
   cur.execute(query)
   rows = cur.fetchall() #the rows is order by room, day, start_time, end_time
                         # so start_time of next row always >= start_time of previous
-  store = dict()
-  for room, day, start, end, wk in rows:
-    if room in store.keys():
-      store[room] += count_time((end-start), wk)   
-    else:
-      #first see this room
-      #calculating total hour normally, and store data into dict 
-      store[room] = count_time((end - start),wk)
-      
-  
+  rooms = reformat(rows)
+
   underused = 0
-  for hours in store.values():
-    if (hours/10) < 20:
-      underused += 1
+  for r in rooms:
+    if r.get_avg_hour() < 20:
+      underused +=1
   
+  print(underused)
   # get total_rooms
   sum_query = '''
   select sum(count) from (
@@ -70,6 +181,31 @@ def count_time(time, wk):
   
   # return as hours
   return hr + extra
+
+def reformat(rows):
+  store = {}
+  for room, day, start, end, wk in rows:
+    if room in store.keys():
+      store[room].add(day, start, end, wk)
+    else:
+      store[room] = Room(room, day, start, end, wk)
+  
+  return list(store.values())
+      
+# def get_no_overlap(start, end, wk, classes):
+#   final_wk = list(wk)
+#   for old_start, old_end, old_wk in classes:
+#     # if overlap
+#     if old_start <= start and start < old_end:
+      
+      
+
+# def extra_time(end, old_end):
+#   extra = end - old_end
+#   if extra <= 0:
+#     return 0
+#   else:
+#     return extra
   
 def connect(key):
   try:
